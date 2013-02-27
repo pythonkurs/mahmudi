400c6f87e29d0a31309bb51d3edd2f6df5c68b6f
owaismahmudi <owais.mahmudi@gmail.com>
2013-02-27 13:10:07 +0100

    updating check_repo.py

--- scripts/check_repo.py
+++ scripts/check_repo.py
@@ -1,20 +1,32 @@
 import sys, os
 from mahmudi.session3 import CourseRepo, ContextManager
 
-if len(sys.argv) != 2:
-    print "Error: Insufficient arguments!\nExpecting:\n check_repo <path>"
-    sys.exit()
+@profile
 
-if not os.path.exists(sys.argv[1]):
-    print "Error: Invalid path!"
-    sys.exit()
+def main():
+    if len(sys.argv) != 2:
+        if sys.argv[1].find('check_repo.py') == -1:
+    	    print "Error: Insufficient arguments!\nExpecting:\n check_repo <path>"
+    	    sys.exit()
 
-thepath = sys.argv[1]
-surname = os.path.basename(thepath)
+    if not os.path.exists(sys.argv[1]):
+        if sys.argv[1].find('check_repo.py') == -1:
+    	    print "Error: Invalid path!"
+    	    sys.exit()
 
-with ContextManager(thepath):
-    obj = CourseRepo(surname)
-    if obj.check():
-        print "PASS"
+    if sys.argv[1].find('check_repo.py') != -1:
+        thepath = sys.argv[2]
     else:
-        print "FAIL"
+        thepath = sys.argv[1]
+    surname = os.path.basename(thepath)
+
+    with ContextManager(thepath):
+        obj = CourseRepo(surname)
+        if obj.check():
+            print "PASS"
+        else:
+            print "FAIL"
+
+
+if __name__ == "__main__":
+    main()
